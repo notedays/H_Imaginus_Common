f2e10f3cd24969d1c397a975d32e7019452a3fff
notedays <notedays@naver.com>
2017-07-25 16:32:26 +0900

    SampleData CityData 추가

---
+++ H_Imaginus_Common/src/com/himaginus/common/data/CityData.java
@@ -0,0 +1,55 @@
+package com.himaginus.common.data;
+
+import com.himaginus.common.data.ResponseData;
+
+public class CityData implements ResponseData {
+
+	private static final long serialVersionUID = 1L;
+
+	private int id;
+	private String name;
+	private String countryCode;
+	private String district;
+	private int population;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCountryCode() {
+		return countryCode;
+	}
+
+	public void setCountryCode(String countryCode) {
+		this.countryCode = countryCode;
+	}
+
+	public String getDistrict() {
+		return district;
+	}
+
+	public void setDistrict(String district) {
+		this.district = district;
+	}
+
+	public int getPopulation() {
+		return population;
+	}
+
+	public void setPopulation(int population) {
+		this.population = population;
+	}
+
+}
